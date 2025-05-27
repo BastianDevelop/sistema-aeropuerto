97d1b9ca881176d3cf09f268caa34f4913965fad
Bastian Develop <blindbastian@gmail.com>
2025-05-26 21:30:02 -0500

    Iniciando con Git y GitHub

---
+++ src/test/java/com/sistema/aeropuerto/SistemaAeropuertoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.aeropuerto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAeropuertoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
